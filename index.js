30d869bfe471048f39e4051ae8e42e59ee37503d
Nathan <dewakik@gmail.com>
2020-10-22 16:04:53 +0200

    [add] node project and ignore node_modules

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log(`script has been loaded successfully`);
+const fetch = require('node-fetch');
 
+let dataRes;
+
+fetch("/secret_data/Survey_Information_Design.json")
+  .then(res => res.json())
+  .then(data => dataRes = data)
+  .then(() => console.log(datRes))
+
+/* 
+  // console.log(`script has been loaded successfully`);
+  let storeData;
+
+  // Fetch local json file
+  fetch("/secret_data/Survey_Information_Design_clean-parsed.json")
+  .then(response => response.json())
+  .then(data => storeData = data)
+  .then(() => console.log(storeData));
+
+  const surveyData = storeData;
+
+  console.log(surveyData)
+*/

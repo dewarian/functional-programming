e05d8576f10989bf938149bb246d13940e749a27
Nathan <nathanbommezijn@gmail.com>
2020-10-27 11:07:33 +0100

    [add] send so I can restart

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const fetch = require('node-fetch');
 
-let dataRes;
+// const fetch = require('node-fetch');
+// const dataSet = "./secret_data/Survey_Information_Design_clean-parsed.json";
+// let datRes;
 
-fetch("/secret_data/Survey_Information_Design.json")
-  .then(res => res.json())
-  .then(data => dataRes = data)
-  .then(() => console.log(datRes))
+// fetch(dataSet)
+//   .then(res => res.json())
+//   .then(data => dataRes = data)
+//   .then(() => console.log(datRes));
 
-/* 
+
   // console.log(`script has been loaded successfully`);
-  let storeData;
+  // let storeData;
 
-  // Fetch local json file
-  fetch("/secret_data/Survey_Information_Design_clean-parsed.json")
-  .then(response => response.json())
-  .then(data => storeData = data)
-  .then(() => console.log(storeData));
+  // // Fetch local json file
+  // fetch("/secret_data/Survey_Information_Design_clean-parsed.json")
+  // .then(response => response.json())
+  // .then(data => storeData = data)
+  // .then(() => console.log(storeData));
 
-  const surveyData = storeData;
+  // const surveyData = storeData;
 
-  console.log(surveyData)
-*/
+  // console.log(surveyData)
+

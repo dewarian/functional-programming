9a9b038a4c770723ff68ff864cbf86990e476d72
Nathan <nathanbommezijn@gmail.com>
2020-10-27 16:26:22 +0100

    [add] retrieve data
    retrieving data from dataset on column basis

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 /* TO DO
-[] require dataset
-[] Zoom into column location (filter dataSet to a single column)
+[x] require dataset
+[x] Zoom into column location (filter dataSet to a single column)
 [] clean a single value
   [] convert coordinates to long lat
 
 */
-let dataSet = require('./secret_data/Survey_Information_Design_clean-parsed.json')
-console.log(getSpecificDataValue(0, "geboorteplaats"))
-testFunction(dataSet)
+let dataSet = require('./secret_data/Survey_Information_Design_clean-parsed.json');
+let copiedData = Object.assign({}, dataSet);
+let testArray = [];
+
+console.log(getSpecificDataValue(1, "geboorteplaats"))
+console.log(filterColumn(copiedData, "geboorteplaats"))
 
 function getSpecificDataValue(key, value) {
   return dataSet[key][value]
 }
 
-function testFunction(object) {
-  for (const geboorteplaats of object) {
-    console.log(geboorteplaats)
+function filterColumn(object, columnName) {
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      const element = object[key];
+      testArray.push(element[columnName])
+    }
   }
+  return testArray
 }

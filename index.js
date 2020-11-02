dbf5a27889f93be840f1bb8554c00360dc6df38e
Nathan <nathanbommezijn@gmail.com>
2020-11-02 17:09:22 +0100

    [add] refactor code Refactored code for surveydata. currently figuring out regex so it doesn't delete the first coordinate.

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('Windows are you still complaining?');
--- survey-assignment/index.js
+++ survey-assignment/index.js
@@ -1,47 +1,63 @@
-/* TO DO
-[x] require dataset
-[x] Zoom into column location (filter dataSet to a single column)
-[] Remove empty values
-[] clean a single value
-  [] convert coordinates to long lat
-
-*/
 // Initializing variables that need to be hoisted
-let dataSet = require('./secret_data/Survey_Information_Design_clean-parsed.json');
-let copiedData = Object.assign({}, dataSet);
-let testArray = [];
-
-console.log(getSpecificDataValue(1, "geboorteplaats"))
-// console.log(filterColumn(copiedData, "geboorteplaats"))
-// console.log(removeEmptyValues(filterColumn(copiedData, "geboorteplaats")))
-
+const dataSet = require('./secret_data/Survey_Information_Design_clean-parsed.json');
+const filteredDataSet = filterDataOnColumn(dataSet, 'geboorteplaats');
 
 /* Return specific data value from element and column */
+/**
+ * @name getSpecificDataValue
+ * @description Retrieves single value based on specificied column and key.
+ * @param {*} key 
+ * @param {*} value Column specified, is equal to name in CSV.
+ */
+/* 
 function getSpecificDataValue(key, value) {
   return dataSet[key][value]
 }
 
-/* Filter dataset and push specified column to new array */
-function filterColumn(object, columnName) {
-  for (const key in object) {
-    if (object.hasOwnProperty(key)) {
-      const element = object[key];
-      testArray.push(element[columnName])
-    }
-  }
-  return testArray
-};
+/* 
+Common shit throughout the data:
+  '',
+  '52.384.222',
+  '52.357302-5.181917',
+  '51.928553. 4.459382',
+  '(51.8878996. 5.6032180)',
+  `"52°30'01.5""N 4°57'47.0""E"`,
 
-/* Check to find empty value in array */
-// const findEmptyValue = (arr) => {
-//   return arr.includes('');
-// };
+  Steps to clean:
+  1. remove empty values
+  2. remove parentheses
+  3. if val contains "" ° NOSW transform 
+  4. if val starts with 52 or 51, check if it has 6 numbers after the dot, transform second dot to comma
+*/
 
-// console.log(findEmptyValue(filterColumn(copiedData, "geboorteplaats")));
+/**
+ * @title Filter data based on column
+ * @description Function retrieves a data value, an object and filters it on the column parameter.
+ * returns a new array
+ * @param {*} data json object
+ * @param {*} column key name
+ */
+function filterDataOnColumn(data, column) {
+  return data.map(result => result[column])
+}
 
-/* function to remove empty values within array */
-function removeEmptyValues(arr) {
-  let arrayWithoutEmptyValues = arr.filter(findEmptyValue)
-  return arrayWithoutEmptyValues;
-};
 
+/**
+ * @title Empty string removal
+ * @description Removes all the strings elements that are empty
+ * With help from Christian C Salvado https://stackoverflow.com/a/281335
+ * Assigned to a variable.
+ */
+const scrubData = filteredDataSet.filter((el) => {
+  return el
+})
+
+
+const cleanData = scrubData.map((location) => {
+  return location
+  .replace('(', '')
+  .replace(')', '')
+  .replace(/\.?\d+.\d+(\.)/, ',')
+})
+
+console.log(cleanData)
